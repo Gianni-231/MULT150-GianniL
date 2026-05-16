4ddac7ed263f3eae2ccfcf333dcc976a2627bc2e
Gianni-231 <glambarri98@gmail.com>
2026-05-16 12:56:51 -0700

    Hour 14 & 16

---
+++ glambarri_hour14and16/Assets/CS/ParticleEffect.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//gave partical a new texture
+//gave partical a material
+// gave partical a sound
+// gave partical a bigger start size 
---
+++ glambarri_hour14and16/Assets/CS/UserInterfaceUserInterface.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// made the title Legends of the Abyss
+// made a unique background 
+// button turns title red for some reason
+// made a weird discription 
