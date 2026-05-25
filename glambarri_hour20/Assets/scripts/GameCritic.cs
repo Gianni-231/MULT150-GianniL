462849860e0cbfd68fa489d56e03fc794b83fa3f
Gianni-231 <glambarri98@gmail.com>
2026-05-25 16:46:16 -0700

    fixed

---
+++ glambarri_hour20/Assets/scripts/GameCritic.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//the game I think is a bit hard but that makes it fun in my opinion
+//I don't think it necessary to make the game easier or harder because instead maybe adding more to it would be a better idea like adding more pickups or making the arena bigger would be better
+//I think adding more pickups and traps would make the game better with each one doing something different
+//I think the game being a bit difficalt made the game fun but I think dodging is fun to do for a bit
+//the thing that makes this game tedious is you doing the same thing so maybe another thing to make the game more appeling is to add something to do in the game.
+
+// I made the powerup into a boot for the futuristic fell and made the obstacle into a spiked wall just because
